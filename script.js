c94d9d9d35c98344febb399570cb55f069eaee6d
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2025-10-03 18:03:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ async function handlePhoneSubmit(e) {
   const submitButton = document.getElementById("submitPhone");
   const phoneRaw = phoneInput.value;
   const phoneClean = phoneRaw.replace(/\D/g, "");
-
+ const agreement = document.getElementById("agreement");
   if (!validatePhone(phoneRaw)) {
     showToast("Введите корректный номер телефона", "", "error");
     phoneInput.classList.add("error");
@@ -99,6 +99,10 @@ async function handlePhoneSubmit(e) {
       return;
     }
      
+     if (!agreement.checked) {
+      showToast("Поставьте галочку для согласия на обработку персональных данных", "", "error")};
+      return;
+    }
   submitButton.value = "Отправка...";
   submitButton.disabled = true;
 
@@ -184,6 +188,7 @@ function showToast(title, description, type = "info") {
     }, 300);
   }, 4000);
 }
+
 
 
 
