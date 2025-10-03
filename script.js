68d924eb1f864f6843034f889c512cc9ff139a4c
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2025-10-03 18:12:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,6 @@ async function handlePhoneSubmit(e) {
   const submitButton = document.getElementById("submitPhone");
   const phoneRaw = phoneInput.value;
   const phoneClean = phoneRaw.replace(/\D/g, "");
- const agr = document.getElementById("agr");
   if (!validatePhone(phoneRaw)) {
     showToast("Введите корректный номер телефона", "", "error");
     phoneInput.classList.add("error");
@@ -185,6 +184,7 @@ function showToast(title, description, type = "info") {
     }, 300);
   }, 4000);
 }
+
 
 
 
