2d74f7d08e622fcee3cdbaf2b8abf128a7814fe7
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2025-10-03 17:58:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -98,11 +98,7 @@ async function handlePhoneSubmit(e) {
       );
       return;
     }
-     const agreement = document.querySelector('.agreement');
-     if (!agreement.checked) {
-      showToast("Поставьте галочку для согласия на обработку персональных данных", "", "error")};
-      return;
-    }
+     
   submitButton.value = "Отправка...";
   submitButton.disabled = true;
 
@@ -188,6 +184,7 @@ function showToast(title, description, type = "info") {
     }, 300);
   }, 4000);
 }
+
 
 
 
